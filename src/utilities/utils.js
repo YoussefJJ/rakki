feef7365d2461682e6136706bf0af62320ac2eb2
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-07 12:18:17 +0100

    add random integer utility function

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -27,3 +27,8 @@ export function getRegionName(region) {
     return regionName;
 }
 
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
