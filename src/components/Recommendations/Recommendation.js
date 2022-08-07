d2d9b2dadbd6db9cab22155f5c898323a73a47ad
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-07 17:20:51 +0100

    add link to recommendation

--- src/components/Recommendations/Recommendation.js
+++ src/components/Recommendations/Recommendation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { getAnimeCover } from '../../utilities/utils'
 import RecContent from './RecContent'
 import RecGenres from './RecGenres'
@@ -8,9 +9,10 @@ import RecTitle from './RecTitle'
 const Recommendation = ({
     data
 }) => {
+  const navigate = useNavigate()
   const genres = data.genres.map(genre => genre).join(', ')
   return (
-    <RecContent>
+    <RecContent onClick={() => navigate(`/anime/${data.id}`)}>
         <RecTitle englishTitle={data.title.english} romajiTitle={data.title.romaji}/>
         <RecImage imageUrl={getAnimeCover(data)} altTitle={data.title.romaji}/>
         <RecGenres genres={genres}/>
