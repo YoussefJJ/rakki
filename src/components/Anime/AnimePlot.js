a3e7a20bbd2ca06c484c91b2963beeaeb7f39aec
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-14 17:59:01 +0100

    change anime plot font size

--- src/components/Anime/AnimePlot.js
+++ src/components/Anime/AnimePlot.js
@@ -4,7 +4,7 @@ function AnimePlot({plot}) {
   return (
     <div className='space-y-3'>
         <h2 className='text-1xl font-bold text-gray-400'>Plot</h2>
-        <p className="text-white text-sm">{plot ? plot : 'No description found for this anime.'}</p>
+        <p className="text-white text-md">{plot ? plot : 'No description found for this anime.'}</p>
     </div>
   )
 }
