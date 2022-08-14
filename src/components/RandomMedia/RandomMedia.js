f6e9b3344ad6518bab81fee0bf81855d0cd7d9d0
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-14 17:58:22 +0100

    add related media component

--- src/components/RandomMedia/RandomMedia.js
+++ src/components/RandomMedia/RandomMedia.js
@@ -1,22 +1,52 @@
+import { useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react'
+import { Link, Outlet, Route, Routes, useNavigate } from 'react-router-dom';
+import { GET_ANIME_LIST } from '../../graphql/queries';
 import { getRandomInt } from '../../utilities/utils'
 
 const RandomMedia = () => {
-  const [mediaId, setMediaId] = useState(1);
+  const navigate = useNavigate()
+  const [randomPage, setRandomPage] = useState(1);
+  const [animeNumber, setAnimeNumber] = useState(1);
+  const [animeList, setAnimeList] = useState([]);
 
   useEffect(() => {
     const getRandomMedia = () => {
-        const min = 1;
-        const max = 20000;
-        const mediaId = getRandomInt(min, max, 1)
-        setMediaId(mediaId)
+        const min = 0;
+        const max = 100;
+        const randomPage = getRandomInt(min, max, 1)
+        const randomId = getRandomInt(0, 49, 1)
+        setRandomPage(randomPage);
+        setAnimeNumber(randomId);
       }
     getRandomMedia()
   }, [])
-  
 
-  return (
-    <div>{mediaId}</div>
+  const {data, loading, error} = useQuery(GET_ANIME_LIST, {
+    variables: {
+        page: randomPage,
+    },
+    onCompleted: (data) => {
+        console.log(data)
+        let number = getRandomInt(0, data.Page.media.length, 1)
+        const animeId = data.Page.media[number].id
+        navigate(`/anime/${animeId}`)
+        // setAnimeList(data.Page.media)
+    },
+    onError: (error) => {
+        console.log(error)
+    }
+  })
+
+//   const animeId = animeList?.[animeNumber]?.id || 1;
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return (<></>
+    // <Link to={`/anime/${animeId}`}>Go to Anime</Link>
+    // <div>{animeList.map(anime => <div>{anime.title.romaji}</div>)}</div>
   )
 }
 
