29af4dee85a9a8b7716b699c2bd95c609cff2782
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-14 17:58:52 +0100

    change anime details font size

--- src/components/Anime/AnimeDetails.js
+++ src/components/Anime/AnimeDetails.js
@@ -11,29 +11,29 @@ function AnimeDetails({
     <div className="text-white w-full bg-slate-700/90 mt-5 rounded p-4">
       {format ? (<div className="flex flex-wrap justify-between items-center">
         <strong>Format: </strong>
-        <span className="text-sm text-gray-300">{format}</span>
+        <span className="text-md text-gray-300">{format}</span>
       </div>): null }
       {duration ? (<div className="flex flex-wrap justify-between items-center">
         <strong>Duration: </strong>
-        <span className="text-sm text-gray-300">
+        <span className="text-md text-gray-300">
           {duration} mins
         </span>
       </div>): null}
       {episodes ? (<div className="flex flex-wrap justify-between items-center">
         <strong>Episodes: </strong>
-        <span className="text-sm text-gray-300">
+        <span className="text-md text-gray-300">
           {episodes}
         </span>
       </div>): null}
       {countryOfOrigin ? (<div className="flex flex-wrap justify-between items-center">
         <strong>Country of Origin: </strong>
-        <span className="text-sm text-gray-300">
+        <span className="text-md text-gray-300">
           {countryOfOrigin}
         </span>
       </div>): null}
       {status ? (<div className="flex flex-wrap justify-between items-center">
         <strong>Status: </strong>
-        <span className="text-sm text-gray-300">{status}</span>
+        <span className="text-md text-gray-300">{status}</span>
         <div className="flex flex-wrap justify-between items-center w-full">
         {externalLinks.length !== 0 ? (
           <>
