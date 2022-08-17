64c7304a108221ad6d03248bcea03abf94ca5667
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-17 19:27:24 +0100

    change button colors

--- src/components/Recommendations/LoadMoreButton.js
+++ src/components/Recommendations/LoadMoreButton.js
@@ -10,8 +10,8 @@ const LoadMoreButton = ({
     <button 
         className="w-full
         flex justify-center items-center
-        p-5 text-darkPurple text-center bg-brightGreen font-bold
-        text-2xl hover:bg-brightPurple hover:text-brightGreen transition-all duration-150 ease-in shadow-md"
+        p-5 text-brightGreen text-center bg-darkPurple font-bold active:bg-darkPurple
+        text-2xl hover:bg-brightPurple transition-all duration-150 ease-in shadow-md"
         onClick={onClick}>
         {loading ? (<Spinner/>): buttonText}
     </button>
