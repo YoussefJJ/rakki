d38de44b9512791c4a5e888d39b5cd2adf93ad07
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-17 19:22:51 +0100

    add custom scrollbar to plot

--- src/components/Anime/AnimePlot.js
+++ src/components/Anime/AnimePlot.js
@@ -4,8 +4,8 @@ function AnimePlot({plot}) {
   return (
     <>
       <h2 className='text-2xl font-bold text-brightPurple'>Plot:</h2>
-      <div className="mt-2 text-sm text-white overflow-auto plot-container">
-        <p className='plot'>{plot ? plot : 'No description found for this anime.'}</p>
+      <div className="mt-2 text-sm text-white overflow-auto plot-container scroll-bar scrollbar-thin scrollbar-thumb-brightGreen scrollbar-track-veryDarkPurple active:scrollbar-thumb-darkGreen">
+        <p className='plot pr-2'>{plot ? plot : 'No description found for this anime.'}</p>
       </div>
     </>
   )
