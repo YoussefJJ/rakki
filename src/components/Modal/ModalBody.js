972c79dba086cf637f40f0e964bdf4f11d0c538c
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-17 19:10:40 +0100

    change scrollbar color

--- src/components/Modal/ModalBody.js
+++ src/components/Modal/ModalBody.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ModalBody = ({children}) => {
   return (
-    <div className="overflow-y-auto w-full scrollbar">
+    <div className="overflow-y-auto w-full scrollbar-thin scroll-bar scrollbar-thumb-brightGreen scrollbar-track-veryDarkPurple active:scrollbar-thumb-darkGreen">
         {children}
     </div>
   )
