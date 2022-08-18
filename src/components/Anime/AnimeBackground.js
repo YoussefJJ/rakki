04fe44801372b6b4ea1f720e5c78fdc4e8cb91ef
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-18 21:56:27 +0100

    fix image centering

--- src/components/Anime/AnimeBackground.js
+++ src/components/Anime/AnimeBackground.js
@@ -4,10 +4,13 @@ const AnimeBackground = ({
     imageUrl
 }) => {
   return (
-    <div
-        className="background-img fixed bg-cover bg-no-repeat w-full h-screen top-0 left-0 -z-50"
-        style={{ backgroundImage: `url('${imageUrl}')` }}
-    ></div>
+    // <div
+    //     className="background-img fixed bg-cover bg-no-repeat w-full h-screen -z-50"
+    //     style={{ backgroundImage: `url('${imageUrl}')` }}
+    // ></div>
+    <img 
+    className='fixed bg-no-repeat object-cover max-w-full h-screen top-0 left-0 right-0 bottom-0 -z-50'
+    src={imageUrl}></img>
   )
 }
 
