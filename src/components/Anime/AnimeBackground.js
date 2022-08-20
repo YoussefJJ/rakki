4ac19eef3f1e7df9b5f705a1a3956b9f089a8d65
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-20 12:39:38 +0100

    fix background image width

--- src/components/Anime/AnimeBackground.js
+++ src/components/Anime/AnimeBackground.js
@@ -3,13 +3,15 @@ import React from 'react'
 const AnimeBackground = ({
     imageUrl
 }) => {
+  const [loading, setLoading] = React.useState(true);
   return (
     // <div
     //     className="background-img fixed bg-cover bg-no-repeat w-full h-screen -z-50"
     //     style={{ backgroundImage: `url('${imageUrl}')` }}
     // ></div>
-    <img 
-    className='fixed bg-no-repeat object-cover max-w-full h-screen top-0 left-0 right-0 bottom-0 -z-50'
+    <img
+    onLoad={() => setLoading(false)}
+    className={`fixed bg-no-repeat object-cover w-full h-screen top-0 left-0 right-0 bottom-0 -z-50 anime-background${loading ? '-loading' : ''}`}
     src={imageUrl}></img>
   )
 }
