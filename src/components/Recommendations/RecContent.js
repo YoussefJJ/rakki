29f06ef031b8e7eaedaf9b5e89c1907ce6abbfab
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-20 12:40:06 +0100

    add animation to recommendation

--- src/components/Recommendations/RecContent.js
+++ src/components/Recommendations/RecContent.js
@@ -7,7 +7,7 @@ const RecContent = ({
   return (
     <div 
     onClick={onClick}
-    className="inline-block object-cover p-1 recommendation-container cursor-pointer" style={{
+    className="inline-block object-cover p-1 recommendation-container cursor-pointer recomm-content" style={{
       width: '95%'
     }}>
         <div className="w-full inline-block border-2 border-brightGreen bg-gray-700 p-0.5 relative">
