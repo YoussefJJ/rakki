7a01ba31d9a808c0075794aef797b6d342ea0735
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-28 14:15:50 +0100

    add loading component

---
+++ src/components/RandomMedia/RandomLoading.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../assets/img/rakki-logo-green.svg'
+import Nagito from '../../assets/img/loading-nagito.gif'
+
+const RandomLoading = ({ text }) => {
+
+  return (
+    <div className='h-[calc(100vh_-_80px)] w-full bg-veryDarkPurple'>
+        <div className='flex flex-col justify-center items-center h-full w-full'>
+            <img src={Logo} alt='rakki-logo' className='loading-image'/>
+            <div className='flex flex-col sm:flex-row justify-between items-center space-y-2 sm:space-y-0'>
+                <img src={Nagito} alt='loading' className='loading-gif'/>
+                <h2 className='ml-5 text-2xl font-medium text-white'>{text}</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RandomLoading
