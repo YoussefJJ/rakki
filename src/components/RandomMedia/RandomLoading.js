900ee45c92787070fa771a09ac62fe7832fb6bbd
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-28 18:11:19 +0100

    fix loading dots animation/import faster gif

--- src/components/RandomMedia/RandomLoading.js
+++ src/components/RandomMedia/RandomLoading.js
@@ -1,20 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Logo from '../../assets/img/rakki-logo-green.svg'
-import Nagito from '../../assets/img/loading-nagito.gif'
+import Nagito from '../../assets/img/loading-nagito-faster.gif'
 
 const RandomLoading = ({ text }) => {
-    const [trail, setTrail] = useState('')
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (trail.length > 3) {
-                setTrail('')
-            } else {
-                setTrail(trail => trail + '.')
-            }
-        } , 300)
-        return () => clearInterval(interval)
-    } , [])
 
   return (
     <div className='h-[calc(100vh_-_80px)] w-full bg-veryDarkPurple'>
@@ -22,7 +10,7 @@ const RandomLoading = ({ text }) => {
             <img src={Logo} alt='rakki-logo' className='loading-image'/>
             <div className='flex flex-col sm:flex-row justify-between items-center space-y-2 sm:space-y-0'>
                 <img src={Nagito} alt='loading' className='loading-gif'/>
-                <h2 className='ml-0 sm:ml-5 text-2xl font-medium text-white'>{`${text}${trail}`}</h2>
+                <span className='ml-0 sm:ml-5 text-2xl font-medium text-white loading-text'>{text}</span>
             </div>
         </div>
     </div>
