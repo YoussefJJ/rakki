20911af940f0b539d0a9c3dff12eee9694cf9b17
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-28 13:00:33 +0100

    add navbar in mobile screen

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,10 @@ import Menu from './Menu'
 
 const Navbar = () => {
   return (
-    <nav className='hidden md:block h-20 bg-veryDarkPurple'>
+    <nav className='h-20 bg-veryDarkPurple'>
         <div className='flex justify-between items-center w-full h-full'>
             <div id="logo_and_links">
                 <div className='flex flex-row justify-start items-center w-full'>
-                    {/* <h1 className='p-2 text-brightGreen text-left text-lg'>Logo</h1> */}
                     <img 
                     src={Logo}
                     style={{
@@ -17,13 +16,6 @@ const Navbar = () => {
                     }}/>
                 </div>
             </div>
-            {/* <div id="menu" className='p-3'>
-                <button className='hamburger'>
-                    <span className='hamburger-top bg-brightGreen'></span>
-                    <span className='hamburger-middle bg-brightGreen'></span>
-                    <span className='hamburger-bottom bg-brightGreen'></span>
-                </button>
-            </div> */}
             <Menu/>
         </div>
     </nav>
