a090836ef5c016644967d1a5b522abe84511c5fc
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-08-28 12:16:27 +0100

    add default link icon path constant

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,8 @@
+import link from '../assets/img/link-not-available.png'
+
 export const YOUTUBE_URL = 'https://www.youtube.com/embed/';
 
 export const DAILYMOTION_URL = 'https://www.dailymotion.com/embed/video/';
+
+export const LINK_DEFAULT = link
 
