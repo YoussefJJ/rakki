036f0084e69ea87af6eb6045d22c5be39f8086b4
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-02 16:56:32 +0100

    change search result style/add animation

--- src/components/Search/Result.js
+++ src/components/Search/Result.js
@@ -1,23 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { getFormat } from '../../utilities/utils'
 
 const Result = ({ media }) => {
   return (
-    <div 
-    className={`group w-full flex space-x-2 border-l-4 border-l-brightGreen \\
-    hover:border-l-white pr-2 hover:text-white hover:bg-brightGreen \\
-    transition-all ease-in`} style={{
-        height: '10vh',
-    }}>
-        <img src={media.coverImage.large} style={{
-            height: 'auto',
-            width: '60px',
-            paddingLeft: '5px',
-            paddingLeft: '5px',
-        }}/>
-        <div className='flex flex-col w-full'>
-            <h1 className='text-md text-brightGreen line-clamp-1 font-bold group-hover:text-white'>{media.title.romaji}</h1>
+    <Link 
+    className='result'
+    onClick={() => document.getElementById('search-modal').click()}
+    to={
+      media.type === 'ANIME' ? `/anime/${media.id}` : `/manga/${media.id}`
+    }>
+    <div className='border-black border-2'>
+      <div 
+      className={`group relative w-full flex space-x-2 border-l-4 bg-veryDarkPurple/70 border-l-brightGreen \\
+      hover:border-l-white pr-2 hover:text-white \\
+      transition-all ease-in`}>
+        <div style={{
+          height: '20vh'
+        }}>
+          <img src={media.coverImage.large} style={{
+              height: '100%',
+              width: '140px',
+              paddingLeft: '5px',
+              paddingLeft: '5px',
+          }}/>
         </div>
+          <div className='flex flex-col w-full justify-between'>
+              <div className='flex flex-col'>
+                <h1 className='text-md text-brightGreen line-clamp-1 font-bold group-hover:underline'>{media.title.romaji}</h1>
+                {media.title.romaji !== media.title.english && <h1 className='text-xs text-brightGreen line-clamp-1 font-bold'>{media.title.english}</h1>}
+                <p className='text-xs text-brightGreen'>{`${getFormat(media.format)} ${(media.chapters || media.episodes) ? 
+                  `(${(media.episodes || media.chapters)} ${media.episodes ? 'episodes' : 'chapters'})`
+                  : ''}`} 
+                </p>
+              </div>
+              <div>
+              <div>
+              </div>
+              </div>
+          </div>
+      </div>
     </div>
+    </Link>
+    // <div 
+    // className='border-4 border-brightGreen' 
+    // style={{
+    //   height: '30vh',
+    //   width: '25vh',
+    // }}
+    // >
+    //   <img className='h-full w-full object-fit' src={media.coverImage.large} alt='image'/>
+    // </div>
   )
 }
 
--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -4,7 +4,7 @@ import Result from './Result'
 const SearchResults = ({ children, results }) => {
   return (
     <div 
-    className='h-full w-full'>
+    className='h-full w-full results'>
         <h1 className='text-brightGreen text-2xl font-bold tracking-wider text-left mb-4'>{children}</h1>
         <div className='w-full flex flex-col space-y-2 overflow-y-auto'>
             {results.map((result, index) => {
