19ea7f67ec69305441527b01fd0b9e42b958c72e
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-08 15:55:25 +0100

    update reducer and context

--- src/reducers/mediaReducer.js
+++ src/reducers/mediaReducer.js
@@ -1,20 +1,38 @@
 export const mediaReducer = (state, action) => {
     switch (action.type) {
-        case 'ADD_MEDIA':
+        case 'ADD_ANIME':
             return {
                 ...state,
-                mediaList: [
+                mediaList: {
                     ...state.mediaList,
-                    action.payload.id
-                ]
+                    anime: [...state.mediaList.anime, action.payload.id]
+                }
             }
-        case 'REMOVE_MEDIA':
+        case 'REMOVE_ANIME':
             return {
                 ...state,
-                mediaList: [
-                    ...state.mediaList.filter(id => id !== action.payload.id)
-                ]
+                mediaList: {
+                    ...state.mediaList,
+                    anime: [...state.mediaList.anime.filter(id => id !== action.payload.id)]
+                }
             }
+        case 'ADD_MANGA':
+            return {
+                ...state,
+                mediaList: {
+                    ...state.mediaList,
+                    manga: [...state.mediaList.manga, action.payload.id]
+                }
+            }
+        case 'REMOVE_MANGA':
+            return {
+                ...state,
+                mediaList: {
+                    ...state.mediaList,
+                    manga: [...state.mediaList.manga.filter(id => id !== action.payload.id)]
+                }
+            }
+
         default:
             return state;
     }
