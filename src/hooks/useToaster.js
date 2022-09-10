2e35354fad4ce5f7b9cee5250aed4b04726f84d0
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-10 14:36:54 +0100

    add clear timeout

--- src/hooks/useToaster.js
+++ src/hooks/useToaster.js
@@ -2,13 +2,19 @@ import { useState } from "react"
 
 export function useToaster () {
     const [show, setShow] = useState(false)
+    const [tOut, setTOut] = useState(null)
     
     const showToaster = () => {
+        if (tOut) {
+            clearTimeout(tOut)
+            setTOut(null)
+        }
         setShow(true)
         console.log("here")
-        setTimeout(() => {
+        const timeout = setTimeout(() => {
             setShow(false)
         }, 3000)
+        setTOut(timeout)
     }
 
     return [show, showToaster]
