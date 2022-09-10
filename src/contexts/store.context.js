f2942a4c7599b36bd1258a98a0a97ac518f33593
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-10 15:08:42 +0100

    add auto localStorage creation

--- src/contexts/store.context.js
+++ src/contexts/store.context.js
@@ -5,13 +5,26 @@ import { mediaReducer } from "../reducers/mediaReducer";
 
 export const MediaContext = createContext();
 
+const checkLocalStorage = (data) => {
+    try {
+        return JSON.parse(data).mediaList && 
+        JSON.parse(data).mediaList.anime && JSON.parse(data).mediaList.manga
+        && Array.isArray(JSON.parse(data).mediaList.anime) && Array.isArray(JSON.parse(data).mediaList.manga)
+
+    } catch (e) {
+        return false;
+    }
+}
+
 const MediaContextProvider = (props) => {
     const [medias, dispatch] = useReducer(mediaReducer, {mediaList: {
         anime: [],
         manga: []
     }}, () => {
         const localData = localStorage.getItem('mediaList');
-        return localData ? JSON.parse(localData) : {
+        //check correct form
+
+        return checkLocalStorage(localData) ? JSON.parse(localData) : {
             mediaList: {
                 anime: [],
                 manga: []
