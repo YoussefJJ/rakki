6d264f111aae1b6d65a6ae99d1d0285976c2292c
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-10 13:54:56 +0100

    add no results found message

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -7,11 +7,11 @@ const SearchResults = ({ children, results }) => {
     className='h-full w-full results'>
         <h1 className='text-brightGreen text-2xl font-bold tracking-wider text-left mb-4'>{children}</h1>
         <div className='w-full flex flex-col space-y-2 overflow-y-auto'>
-            {results.map((result, index) => {
+            {results.length > 0 ? results.map((result, index) => {
                 return (
                     <Result key={index} media={result}/>
                 )}
-            )}
+            ): <h1 className='text-brightGreen text-lg mt-4 font-bold tracking-wider text-left'>No results found</h1>}
         </div>
     </div>
   )
