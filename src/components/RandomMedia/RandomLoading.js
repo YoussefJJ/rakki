7ddf7645bf6c9de20629fa937cd96c10f83f1810
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-16 19:28:25 +0100

    change loading text font

--- src/components/RandomMedia/RandomLoading.js
+++ src/components/RandomMedia/RandomLoading.js
@@ -9,7 +9,7 @@ const RandomLoading = ({ text }) => {
             {/* <img src={Logo} alt='rakki-logo' className='loading-image'/> */}
             <div className='flex flex-col sm:flex-row justify-between items-center space-y-2 sm:space-y-0'>
                 <img src={Nagito} alt='loading' className='loading-gif'/>
-                <span className='ml-0 sm:ml-5 text-2xl font-medium text-white loading-text'>{text}</span>
+                <span className='ml-0 sm:ml-5 text-2xl font-medium text-white loading-text font-osaka'>{text}</span>
             </div>
         </div>
     </div>
