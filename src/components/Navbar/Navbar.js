527207ad22c371785317e608a0aa8b20885aa663
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-16 10:14:30 +0100

    change responsive design layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,15 +7,15 @@ const Navbar = () => {
   return (
     <nav className='h-20 bg-veryDarkPurple relative overflow-hidden'>
         <div className='flex justify-between items-center w-full h-full'>
-            <div id="logo_and_links" className='flex items-center px-5 md:relative md:w-1/3'>
+            <div id="logo_and_links" className='flex justify-between items-center px-5 md:relative w-1/2 md:w-1/3'>
                 <Menu/>
-                <div className='flex flex-row justify-start items-center w-full md:absolute md:left-0 md:right-0 md:m-auto' style={{
-                    height: '100px',
+                <div className='w-full md:absolute md:left-0 md:right-0 md:m-auto' style={{
+                    height: '80px',
                     width: '100%',
                     }}>
                     <img 
                     src={Logo}
-                    className='md:w-fit md:m-auto h-full logo'
+                    className='md:w-fit m-auto h-full logo'
                     />
                 </div>
             </div>
