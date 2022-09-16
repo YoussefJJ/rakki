f9589a4e8db0c0fe44c0ca64cb064ab23beea63f
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-09-16 09:46:40 +0100

    change navbar elements design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,22 +7,23 @@ const Navbar = () => {
   return (
     <nav className='h-20 bg-veryDarkPurple relative overflow-hidden'>
         <div className='flex justify-between items-center w-full h-full'>
-            <div id="logo_and_links">
-                <div className='flex flex-row justify-start items-center w-full logo'>
+            <div id="logo_and_links" className='flex items-center px-5 md:relative md:w-1/3'>
+                <Menu/>
+                <div className='flex flex-row justify-start items-center w-full md:absolute md:left-0 md:right-0 md:m-auto' style={{
+                    height: '100px',
+                    width: '100%',
+                    }}>
                     <img 
                     src={Logo}
-                    style={{
-                    height: '100px',
-                    width: '200px',
-                    }}/>
+                    className='md:w-fit md:m-auto h-full logo'
+                    />
                 </div>
             </div>
             <div className='h-1.5 absolute bottom-0 w-full bg-brightGreen -skew-x-[28deg] translate-x-1/2 md:translate-x-1/3'>
 
             </div>
-            <div className='flex'>
+            <div className='flex px-5'>
               <Search/>
-              <Menu/>
             </div>
         </div>
     </nav>
